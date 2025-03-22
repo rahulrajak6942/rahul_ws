b951f2139d903ee3da8660675c38e126acd0acac
rahulrajak6942 <134321959+rahulrajak6942@users.noreply.github.com>
2025-03-22 17:56:29 +0530

    Update setup.py

--- src/bot_world/setup.py
+++ src/bot_world/setup.py
@@ -20,7 +20,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='tirth',
-    maintainer_email='tirth.vyas2003@gmail.com',
+    maintainer_email='rahulrajak5629@gmail.com',
     description='TODO: Package description',
     license='Apache-2.0',
     tests_require=['pytest'],
